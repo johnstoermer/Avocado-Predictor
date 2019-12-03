ff8135179a00eb1dcda5770c5b088c6d433ad102
John Stoermer <jstoerm@purdue.edu>
2019-12-02 23:39:35 -0500

    did kfold

--- avocado_project.py
+++ avocado_project.py
@@ -19,32 +19,48 @@ def load_df(f):
     df['type'] = df['type'].apply(lambda x: 0 if x == 'conventional' else 1)
     df = df.rename(columns={'Date': 'month'})
     df = pd.get_dummies(df)
-    df = df.sample(frac=1)
     return df
 
-if __name__ == "__main__":
-
-    df = load_df('avocado.csv')
-
-    print(df)
-    print(len(df.columns))
-
-    Xtrain = df.values[:,1:]
-    ytrain = df.values[:,0]
-
+def train_it(Xtrain, ytrain):
     avocado_model = tf.keras.models.Sequential([
       tf.keras.layers.Dense(58, input_dim=58, activation='relu'),
       tf.keras.layers.Dense(58, input_dim=58, activation='relu'),
       tf.keras.layers.Dense(1)
     ])
-
-    avocado_model.compile(optimizer='adam',
-              loss='mse',
-              metrics=['mae'])
-
+    avocado_model.compile(optimizer='adam', loss='mse', metrics=['mae'])
     avocado_model.fit(Xtrain, ytrain, epochs=100)
+    return avocado_model
 
-    prediction = avocado_model.predict(Xtrain)
+def test_it(model, Xtest, ytest):
+    prediction = model.predict(Xtest)
+    train_error =  (ytest - prediction) ** 2
+    return np.mean(train_error)
 
-    train_error =  (ytrain - prediction) ** 2
-    mean_sq_error = np.mean(train_error)
+'''
+Shuffle the dataset randomly.
+Split the dataset into k groups
+For each unique group:
+Take the group as a hold out or test data set
+Take the remaining groups as a training data set
+Fit a model on the training set and evaluate it on the test set
+Retain the evaluation score and discard the model
+Summarize the skill of the model using the sample of model evaluation scores
+'''
+def kfold_it(df): #k = 5
+    df = df.sample(frac=1)
+    folds = np.array_split(df, 5)
+    fold_mses = []
+    for i in range(5):
+        test_df = folds[i]
+        train_df = pd.concat([folds[j] for j in range(5) if j != i], axis=0)
+        model = train_it(train_df.values[:,1:], train_df.values[:,0])
+        fold_mse = test_it(model, test_df.values[:,1:], test_df.values[:,0])
+        print('Fold ' + str(i) + ' MSE: ' + str(fold_mse))
+        fold_mses.append(fold_mse)
+    return np.mean(fold_mses), np.std(fold_mses)
+
+
+if __name__ == "__main__":
+
+    df = load_df('avocado.csv')
+    print(kfold_it(df))
